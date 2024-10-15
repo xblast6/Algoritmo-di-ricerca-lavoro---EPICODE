1a81a14d122a7b00ed640a74e2971449b2dc496c
xblast6 <98alecarnevale@gmail.com>
2024-10-15 17:41:48 +0200

    Update script.js
    Ho tolto la traccia dell'esercizio da sopra la funzione

--- Esercizio di Fine Modulo/script.js
+++ Esercizio di Fine Modulo/script.js
@@ -123,65 +123,7 @@ const jobs = [
     },
   ]
   
-  /*PARTE 1: 
-Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
-Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
-Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
-L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
-Prendi queste tre inserzioni ad esempio:
 
-      job1:  location: "NY, US",     title: "java dev"
-      job2:  location: "Genoa, IT"   title: "web dev"
-      job3:  location: "US"      title: "dev"
-
-Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
-in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
-
-REQUISITI:
-- il tuo algoritmo deve tornare i risultati nella seguente forma:
-{
-  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
-  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
-}
-
-- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate).
-Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
-PARTE 2: 
-Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
-
-Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
-
-Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
-    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
-    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
-    SUGGERIMENTO: ti servira’ un ciclo for!
-
-*/
-    /*let listaRisultati = document.getElementById("listaRisultati")
-    let posLavorativa = document.getElementById("ricercaLavoro").value
-    let posGeografica = document.getElementById("ricercaPosizione").value
-function ricercaLavoro(posLavorativa, posGeografica) {
-    
-    console.log(posLavorativa + posGeografica);
-    
-    posLavorativa = posLavorativa.toLowerCase()
-    posGeografica = posGeografica.toLowerCase()
-    let result = []
-    for (let i = 0; i < jobs.length; i++) {
-      let titleMinuscolo = jobs[i].title.toLowerCase()
-      let locationMinuscolo = jobs[i].location.toLowerCase()
-      if (titleMinuscolo.includes(posLavorativa) && locationMinuscolo.includes(posGeografica)) {
-        result.push(jobs[i])
-      }
-    }
-    if (result.length === 0) {
-      return "Non ci sono risultati per la tua ricerca"
-    }
-    let count = result.length
-    listaRisultati.appendChild("<li>" + result.title + result.location + "</li>")
-    
-}
-*/
 
 let posLavorativa = document.getElementById("ricercaLavoro")
 let posGeografica = document.getElementById("ricercaPosizione")
